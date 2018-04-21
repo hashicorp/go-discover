bee8784115b98328982ccc4d4e327214843b347e
Jack Pearkes <jackpearkes@gmail.com>
2018-04-20 23:11:27 -0700

    tests/google: share credentials and simply tf

--- test/tf/gce/input.tf
+++ test/tf/gce/input.tf
@@ -1,17 +1,4 @@
-variable "region" {
-  default = "europe-west1"
-}
-
-variable "region_zone" {
-  default = "europe-west1-c"
-}
-
-variable "project_name" {
-  description = "The ID of the Google Cloud project"
-  default     = ""
-}
-
 variable "credentials_file_path" {
   description = "Path to the JSON file used to describe your account credentials"
-  default     = "tf_gce.json"
+  default     = "credentials.json"
 }
--- test/tf/gce/main.tf
+++ test/tf/gce/main.tf
@@ -1,19 +1,40 @@
-provider "google" {
-  region      = "${var.region}"
-  project     = "${var.project_name}"
-  credentials = "${file("${var.credentials_file_path}")}"
-}
+provider "google" {}
 
-resource "google_compute_instance" "main" {
+resource "google_compute_instance" "tagged" {
   count = 2
 
   name         = "tf-discover-${count.index}"
   machine_type = "f1-micro"
-  zone         = "${var.region_zone}"
-  tags         = ["ssh-node", "consul-${count.index}"]
+  tags         = ["consul-server"]
 
-  disk {
-    image = "ubuntu-os-cloud/ubuntu-1404-trusty-v20160602"
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1404-trusty-v20160602"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      # Ephemeral
+    }
+  }
+
+  service_account {
+    scopes = ["https://www.googleapis.com/auth/compute.readonly"]
+  }
+}
+
+resource "google_compute_instance" "untagged" {
+  name         = "tf-discover-untagged"
+  machine_type = "f1-micro"
+  tags         = ["consul-not-matching"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1404-trusty-v20160602"
+    }
   }
 
   network_interface {
