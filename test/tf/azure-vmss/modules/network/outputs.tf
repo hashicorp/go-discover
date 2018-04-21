3d1d86e458ff7b3bbfa81c4c2c7bcca577da15a8
Jack Pearkes <jackpearkes@gmail.com>
2018-04-20 21:47:21 -0700

    tests: remove outpout files
    Without the test helpers these aren't as useful anymore and just
    end up being more references in the Terraform configuration,
    so opting to remove them for now.

--- test/tf/azure-vmss/modules/network/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "subnet_id" {
-  value = "${azurerm_subnet.internal.id}"
-}
--- test/tf/azurem/modules/virtual_machine/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_ip" {
-  value = "${azurerm_public_ip.external.ip_address}"
-}
-
-output "private_ip" {
-  value = "${azurerm_network_interface.internal.private_ip_address}"
-}
