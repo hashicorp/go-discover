077e76004526f6e9397650562abbe79e791c5444
Jack Pearkes <jackpearkes@gmail.com>
2018-04-20 19:32:19 -0700

    provider/azure: switch azurerm tags to the ones used in the TF config
    This matches the tags specified in the `test/tf` Terraform.

--- provider/azure/azure_discover_test.go
+++ provider/azure/azure_discover_test.go
@@ -12,8 +12,8 @@ import (
 func TestTagAddrs(t *testing.T) {
 	args := discover.Config{
 		"provider":          "azure",
-		"tag_name":          "type",
-		"tag_value":         "Foundation",
+		"tag_name":          "consul",
+		"tag_value":         "server",
 		"subscription_id":   os.Getenv("ARM_SUBSCRIPTION_ID"),
 		"tenant_id":         os.Getenv("ARM_TENANT_ID"),
 		"client_id":         os.Getenv("ARM_CLIENT_ID"),
