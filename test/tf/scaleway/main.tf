83b2d0ddf9fa3812552ea23688a0820fe413e9c5
Siva <siva@Sivas-MacBook-Pro.local>
2018-05-30 18:46:51 -0400

    terraform fmt on Scaleway Directory

--- test/tf/scaleway/main.tf
+++ test/tf/scaleway/main.tf
@@ -2,16 +2,18 @@
 provider "scaleway" {
   region = "${var.region}"
 }
+
 resource "scaleway_server" "test" {
   count = 2
   image = "${var.image}"
   type  = "C2S"
-  tags = ["consul-server"]
-  name = "test-server"
+  tags  = ["consul-server"]
+  name  = "test-server"
 }
+
 resource "scaleway_server" "dummy" {
   image = "${var.image}"
-  name = "dummy_scaleway_instance"
-  type = "C2S"
-  tags = ["Dummy"]
+  name  = "dummy_scaleway_instance"
+  type  = "C2S"
+  tags  = ["Dummy"]
 }
