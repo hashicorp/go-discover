a8b195e829b298505186a8f267075a2e201ae314
Siva <sivaauturic@gmail.com>
2018-05-31 21:02:03 -0400

    Terraform files for spinning up Alicloud instance

--- test/tf/aliyun/main.tf
+++ test/tf/aliyun/main.tf
@@ -2,14 +2,12 @@ provider "alicloud" {}
 
 resource "alicloud_instance" "test" {
   image_id        = "centos_7_04_64_20G_alibase_201701015.vhd"
-  availability_zone = "us-west-1"
-  instance_type   = "ecs.g5.large"
-  security_groups = ["${alicloud_security_group.sg.id}"]
+  instance_type   = "${var.instance_type}"
+  security_groups = ["${alicloud_security_group.default.id}"]
   tags            = "${var.tag}"
 }
 
-resource "alicloud_security_group" "sg" {
-  id          = "default"
-  description = "Default"
-  vpc_id      = "test_0315"
+resource "alicloud_security_group" "default" {
+  name        = "default"
+  description = "default"
 }
--- test/tf/aliyun/variables.tf
+++ test/tf/aliyun/variables.tf
@@ -1,6 +1,11 @@
 variable "tag" {
   type = "map"
+
   default = {
     "consul" = "server.test"
   }
+}
+
+variable "instance_type" {
+  default = "ecs.n4.small"
 }
