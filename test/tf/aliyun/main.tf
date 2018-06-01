f813bf45d00135c1a039e028fec38b7cc387ddbc
Siva Prasad <MagnumOpus21@users.noreply.github.com>
2018-06-01 13:32:19 -0400

    Added tests for AliCloud provider (#41)
    * AliCloud Skeletons
    
    * AliCloud Definitions
    
    * Aliyun environmental variables changed to match synchrony with Terraform
    
    * Terraform files for spinning up Alicloud instance
    
    * terraform fmt on the test/tf directory
    
    * Image_id is a variable now. Updated README.md

---
+++ test/tf/aliyun/main.tf
@@ -0,0 +1,14 @@
+provider "alicloud" {}
+
+resource "alicloud_instance" "test" {
+  count           = 2
+  image_id        = "${var.image_id}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["${alicloud_security_group.default.id}"]
+  tags            = "${var.tag}"
+}
+
+resource "alicloud_security_group" "default" {
+  name        = "default"
+  description = "default"
+}
---
+++ test/tf/aliyun/variables.tf
@@ -0,0 +1,15 @@
+variable "tag" {
+  type = "map"
+
+  default = {
+    "consul" = "server.test"
+  }
+}
+
+variable "instance_type" {
+  default = "ecs.n4.small"
+}
+
+variable "image_id" {
+  default = "centos_7_04_64_20G_alibase_201701015.vhd"
+}
