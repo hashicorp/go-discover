007e3777dff267d140a6ca40c819c3a4c46af953
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2018-08-02 08:55:37 -0700

    Remove the integ test, we'll use TF for that

--- provider/k8s/k8s_discover_test.go
+++ provider/k8s/k8s_discover_test.go
@@ -14,27 +14,6 @@ import (
 
 var _ discover.Provider = (*k8s.Provider)(nil)
 
-func TestAddrs(t *testing.T) {
-	args := discover.Config{
-		"provider":       "k8s",
-		"label_selector": "app=consul-server",
-	}
-
-	l := log.New(os.Stderr, "", log.LstdFlags)
-	p := &k8s.Provider{}
-	addrs, err := p.Addrs(args, l)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Logf("Result: %v", addrs)
-
-	// This is a weak assertion, but given the dynamic scheduling of
-	// Consul in a K8S cluster, its hard to expect specific IP addresses.
-	if len(addrs) != 3 {
-		t.Fatalf("expected 3 results, got %v", addrs)
-	}
-}
-
 func TestPodAddrs(t *testing.T) {
 	cases := []struct {
 		Name     string
