4519c53251c4934733c30f66dc2f41e9b276ebb3
Steven Jacobs <stjacobs@fastmail.fm>
2019-03-31 10:15:06 -0400

    add random test id with more test cases

--- provider/linode/linode_discover_test.go
+++ provider/linode/linode_discover_test.go
@@ -12,24 +12,105 @@ import (
 var _ discover.Provider = (*linode.Provider)(nil)
 var _ discover.ProviderWithUserAgent = (*linode.Provider)(nil)
 
-func TestAddrs(t *testing.T) {
+func TestAddrsTagedDefault(t *testing.T) {
 	args := discover.Config{
 		"provider":  "linode",
-		"tag_name":  "go-discover-test-tag",
-		"region":    "us-east",
 		"api_token": os.Getenv("LINODE_TOKEN"),
+		"tag_name":  "gd-tag1",
 	}
+
 	if args["api_token"] == "" {
 		t.Skip("Linode credentials missing")
 	}
 
-	l := log.New(os.Stderr, "", log.LstdFlags)
 	p := &linode.Provider{}
+
+	l := log.New(os.Stderr, "", log.LstdFlags)
 	addrs, err := p.Addrs(args, l)
+
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if len(addrs) != 2 {
+		t.Fatalf("bad: %v", addrs)
+	}
+}
+
+func TestAddrsTaggedPublicV6(t *testing.T) {
+	args := discover.Config{
+		"provider":     "linode",
+		"api_token":    os.Getenv("LINODE_TOKEN"),
+		"address_type": "public_v6",
+		"tag_name":     "gd-tag1",
+	}
+
+	if args["api_token"] == "" {
+		t.Skip("Linode credentials missing")
+	}
+
+	p := &linode.Provider{}
+
+	l := log.New(os.Stderr, "", log.LstdFlags)
+	addrs, err := p.Addrs(args, l)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addrs) != 2 {
+		t.Fatalf("bad: %v", addrs)
+	}
+}
+
+func TestAddrsTaggedPublicV4(t *testing.T) {
+	args := discover.Config{
+		"provider":     "linode",
+		"api_token":    os.Getenv("LINODE_TOKEN"),
+		"address_type": "public_v4",
+		"tag_name":     "gd-tag1",
+	}
+
+	if args["api_token"] == "" {
+		t.Skip("Linode credentials missing")
+	}
+
+	p := &linode.Provider{}
+
+	l := log.New(os.Stderr, "", log.LstdFlags)
+	addrs, err := p.Addrs(args, l)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addrs) != 2 {
+		t.Fatalf("bad: %v", addrs)
+	}
+}
+
+func TestAddrsTaggedRegion(t *testing.T) {
+	args := discover.Config{
+		"provider":  "linode",
+		"api_token": os.Getenv("LINODE_TOKEN"),
+		"tag_name":  "gd-tag1",
+		"region":    "us-east",
+	}
+
+	if args["api_token"] == "" {
+		t.Skip("Linode credentials missing")
+	}
+
+	p := &linode.Provider{}
+
+	l := log.New(os.Stderr, "", log.LstdFlags)
+	addrs, err := p.Addrs(args, l)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(addrs) != 1 {
 		t.Fatalf("bad: %v", addrs)
 	}
 }
--- test/tf/linode/main.tf
+++ test/tf/linode/main.tf
@@ -2,20 +2,29 @@ provider "linode" {
   version = "~> 1.5.0"
 }
 
-resource "linode_instance" "tagged" {
-  count              = 2
-  image              = "${var.linode_image}"
-  label              = "${var.prefix}-tagged-${count.index}"
-  region             = "${var.linode_region}"
-  type               = "${var.linode_type}"
-  private_ip         = true
-  tags               = ["${var.linode_tag}"]
+provider "random" {
+  version = "~> 2.0.0"
 }
 
-resource "linode_instance" "untagged" {
-  image              = "${var.linode_image}"
-  label              = "${var.prefix}-untagged"
-  region             = "${var.linode_region}"
-  type               = "${var.linode_type}"
-  private_ip         = true
+resource "random_id" "test_id" {
+  byte_length = 4
+  prefix = "go-discover-"
+}
+
+variable "regions" {
+  default = ["us-east", "ap-south", "ap-south"]
+}
+
+variable "tags" {
+  default = ["gd-tag1", "gd-tag1", "gd-tag2"]
+}
+
+resource "linode_instance" "go-discover-linode" {
+  count      = 3
+  image      = "linode/debian9"
+  label      = "${random_id.test_id.dec}-${count.index}"
+  region     = "${element(var.regions, count.index)}"
+  type       = "g6-nanode-1"
+  private_ip = true
+  tags       = ["${element(var.tags, count.index)}"]
 }
