62920d8899d447f9ffb28b1829993cd92708153e
Li Kexian <doyenli@tencent.com>
2019-09-02 19:59:05 +0800

    add provider implementations of tencentcloud

---
+++ test/tf/tencentcloud/main.tf
@@ -0,0 +1,16 @@
+provider "tencentcloud" {
+  version = ">= 1.10.0"
+}
+
+resource "tencentcloud_instance" "test" {
+  count                      = 2
+  instance_name              = "test"
+  availability_zone          = "${var.availability_zone}"
+  image_id                   = "${var.image_id}"
+  instance_type              = "${var.instance_type}"
+  system_disk_type           = "CLOUD_PREMIUM"
+  system_disk_size           = 50
+  internet_max_bandwidth_out = 1
+  allocate_public_ip         = true
+  tags                       = "${var.tag}"
+}
