beadc488f51180af71d2483d1d92fdcaf92a5e7c
oogy <notan@email.com>
2019-10-13 16:48:55 +0000

    added Vultr TF test

---
+++ test/vultr/main.tf
@@ -0,0 +1,13 @@
+provider "vultr" {
+
+}
+
+resource "vultr_server" "test" {
+  count			= 2
+  plan_id		= "201"
+  region_id		= "1"
+  os_id			= "167"
+  label			= "go-discover-test-${count.index}"
+  tag			= "go-discover-test-tag"
+  enable_private_network = true
+}
