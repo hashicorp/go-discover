3b643c671c3622b28c8ba3e5f4e918e36989e063
Gio d'Amelio <giodamelio@gmail.com>
2021-02-28 14:24:47 -0600

    Get the IP addresses from the pool vms

--- provider/proxmox/proxmox_discover.go
+++ provider/proxmox/proxmox_discover.go
@@ -3,7 +3,6 @@ package proxmox
 import (
 	"fmt"
 	"log"
-	"net/http"
 )
 
 // Provider for Proxmox
@@ -18,7 +17,7 @@ func (p *Provider) Help() string {
 		api_token_id:        The ID of the API token
 		api_token_secret:    The secret of the API token
 		api_skip_tls_verify: "skip" or "verify". Defaults to "verify"
-		addr_type:           "v4", "v6" or "both". Defaults to "v4".
+		addr_type:           "v4" or "v6". Defaults to "v4".
 		pool_name            Pool to get VMs from
 `
 }
@@ -33,12 +32,58 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 		args["api_skip_tls_verify"] = "verify"
 	}
 
-	_, err := http.Get("")
-	if err != nil {
-		return nil, fmt.Errorf("discover-proxmox: %s", err)
+	if args["addr_type"] != "v4" && args["addr_type"] != "v6" {
+		args["addr_type"] = "v4"
 	}
 
-	addrs := []string{"haha", "123"}
+	// Get all the members of the pool
+	members, err := getPoolMembers(args)
+	if err != nil {
+		return nil, fmt.Errorf("discover-proxmox: could not list pool members: %s", err)
+	}
 
-	return addrs, nil
+	// Get the network interfaces from just the members that at QEMU vm's
+	var interfaces []networkInterface
+	for _, member := range members {
+		if member.Type == "qemu" {
+			memberInterfaces, err := getNetworkInterfaces(args, member.Node, fmt.Sprint(member.VMID))
+			if err != nil {
+				return nil, fmt.Errorf(
+					"discover-proxmox: could not get interfaces from pool member '%s' (ID: %d): %s",
+					member.Name,
+					member.VMID,
+					err,
+				)
+			}
+
+			// Add the first non loopback interface to the output list
+			for _, memberInterface := range memberInterfaces {
+				// Ignore loopback interfaces
+				if memberInterface.HardwareAddress == "00:00:00:00:00:00" {
+					continue
+				}
+
+				interfaces = append(interfaces, memberInterface)
+				break
+			}
+		}
+	}
+
+	// Collect the correct (ipv4 or ipv6) IP addresses from the interface
+	var addresses []string
+	for _, netInterface := range interfaces {
+		for _, ipAddress := range netInterface.IPAddresses {
+			if args["addr_type"] == "v4" && ipAddress.IPAddressType == "ipv4" {
+				addresses = append(addresses, ipAddress.IPAddress)
+				break
+			}
+
+			if args["addr_type"] == "v6" && ipAddress.IPAddressType == "ipv6" {
+				addresses = append(addresses, ipAddress.IPAddress)
+				break
+			}
+		}
+	}
+
+	return addresses, nil
 }
--- provider/proxmox/proxmox_discover_test.go
+++ provider/proxmox/proxmox_discover_test.go
@@ -1,7 +1,91 @@
 package proxmox
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"testing"
+
 	discover "github.com/hashicorp/go-discover"
 )
 
 var _ discover.Provider = (*Provider)(nil)
+
+var (
+	ipv4Regex, _ = regexp.Compile(`^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`)
+	ipv6Regex, _ = regexp.Compile(`^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`)
+)
+
+func TestAddrs(t *testing.T) {
+	args := discover.Config{
+		"provider":            "proxmox",
+		"api_host":            os.Getenv("PROXMOX_API_HOST"),
+		"api_token_id":        os.Getenv("PROXMOX_API_ID"),
+		"api_token_secret":    os.Getenv("PROXMOX_API_SECRET"),
+		"api_skip_tls_verify": "skip",
+		"pool_name":           os.Getenv("PROXMOX_POOL_NAME"),
+	}
+
+	if args["api_host"] == "" || args["api_token_id"] == "" || args["api_token_secret"] == "" || args["pool_name"] == "" {
+		t.Skip("Proxmox credentials missing")
+	}
+
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	p := &Provider{}
+	addrs, err := p.Addrs(args, logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Addresses recieved:", addrs)
+
+	// Assume that at least one address was returned
+	if len(addrs) <= 0 {
+		t.Fatal("Zero addresses returned")
+	}
+
+	// Ensure the addresses returned were ipv4 addresses
+	for _, address := range addrs {
+		if !ipv4Regex.MatchString(address) {
+			t.Fatalf("IP address is not a valid ipv4 address: %s", address)
+		}
+	}
+}
+
+func TestAddrsIPv6(t *testing.T) {
+	args := discover.Config{
+		"provider":            "proxmox",
+		"api_host":            os.Getenv("PROXMOX_API_HOST"),
+		"api_token_id":        os.Getenv("PROXMOX_API_ID"),
+		"api_token_secret":    os.Getenv("PROXMOX_API_SECRET"),
+		"api_skip_tls_verify": "skip",
+		"pool_name":           os.Getenv("PROXMOX_POOL_NAME"),
+		"addr_type":           "v6",
+	}
+
+	if args["api_host"] == "" || args["api_token_id"] == "" || args["api_token_secret"] == "" || args["pool_name"] == "" {
+		t.Skip("Proxmox credentials missing")
+	}
+
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	p := &Provider{}
+	addrs, err := p.Addrs(args, logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("Addresses recieved:", addrs)
+
+	// Assume that at least one address was returned
+	if len(addrs) <= 0 {
+		t.Fatal("Zero addresses returned")
+	}
+
+	// Ensure the addresses returned were ipv4 addresses
+	for _, address := range addrs {
+		if !ipv6Regex.MatchString(address) {
+			t.Fatalf("IP address is not a valid ipv6 address: %s", address)
+		}
+	}
+}
