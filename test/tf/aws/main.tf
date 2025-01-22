97d013dfed53335a04f2ee0d94985bcbddfce6a5
Mohan Manikanta <mohan.manikanta@hashicorp.com>
2025-01-22 17:23:02 +0530

    IND-1931 TF Test ubuntu image and AWS region updated.

--- test/tf/aws/main.tf
+++ test/tf/aws/main.tf
@@ -9,7 +9,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
 
   filter {
