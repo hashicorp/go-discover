41d519f1e0cd64daba8bcc84ff6cf74f7c083f61
Tanmay Garg <tanmay17061@gmail.com>
2025-05-20 10:58:11 +0530

    aws: enable dual stack for non-default credential chain

--- provider/aws/aws_discover.go
+++ provider/aws/aws_discover.go
@@ -140,8 +140,11 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 	if accessKey != "" && secretKey != "" {
 		l.Printf("[INFO] discover-aws: Using static credentials provider")
 		staticCreds := credentials.NewStaticCredentialsProvider(accessKey, secretKey, sessionToken)
-		cfg, err = config.LoadDefaultConfig(context.TODO(), config.WithRegion(region),
-			config.WithCredentialsProvider(aws.NewCredentialsCache(staticCreds)))
+		cfg, err = config.LoadDefaultConfig(context.TODO(),
+			config.WithRegion(region),
+			config.WithUseDualStackEndpoint(aws.DualStackEndpointStateEnabled),
+			config.WithCredentialsProvider(aws.NewCredentialsCache(staticCreds)),
+		)
 		if err != nil {
 			l.Printf("[INFO] discover-aws: unable to load SDK config with Static Provider, %v", err)
 		}
