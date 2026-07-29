f7d3c15f7f69d833169353b00d84df95bd401e1c
O Seibert Syseleven <36202147+oseiberts11@users.noreply.github.com>
2026-07-29 16:45:45 +0200

    openstack: don't look up VMs by project ID. (#265)
    There is no need to specify the project ID to find VMs. By default you only see
    the VMs of the project that you authenticated with anyway.  (And there was no
    way in the discover code to auth with one project and look up another.) Finding
    the ID is annoying if you want to auth by project name, which was not even
    possible before.  Since we don't *need* to obtain the ID, we can get rid of the
    somewhat ususal way to obtain it from the metadata service if neither the
    project name nor the ID is given, either as an argument or in the customary
    OpenStack environment variables.

--- provider/os/os_discover.go
+++ provider/os/os_discover.go
@@ -1,4 +1,4 @@
-// Copyright IBM Corp. 2017, 2025
+// Copyright IBM Corp. 2017, 2026
 // SPDX-License-Identifier: MPL-2.0
 
 // Package os provides node discovery for Openstack.
@@ -35,6 +35,7 @@ func (p *Provider) Help() string {
     provider:   "os"
     auth_url:   The endpoint of OS identity
     project_id: The id of the project (tenant id)
+    project_name: The name of the project (tenant name)
     tag_key:    The tag key to filter on
     tag_value:  The tag value to filter on
     user_name:  The user used to authenticate
@@ -55,22 +56,11 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 		l = log.New(io.Discard, "", 0)
 	}
 
-	projectID := args["project_id"]
 	tagKey := args["tag_key"]
 	tagValue := args["tag_value"]
 	var err error
 
-	if projectID == "" { // Use the one on the instance if not provided either by parameter or env
-		l.Printf("[INFO] discover-os: ProjectID not provided. Looking up in metadata...")
-		projectID, err = getProjectID()
-		if err != nil {
-			return nil, err
-		}
-		l.Printf("[INFO] discover-os: ProjectID is %s", projectID)
-		args["project_id"] = projectID
-	}
-
-	log.Printf("[DEBUG] discover-os: Using project_id=%s tag_key=%s tag_value=%s", projectID, tagKey, tagValue)
+	log.Printf("[DEBUG] discover-os: Using tag_key=%s tag_value=%s", tagKey, tagValue)
 	client, err := newClient(args, l)
 	if err != nil {
 		return nil, err
@@ -80,7 +70,7 @@ func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error
 		client.UserAgent.Prepend(p.userAgent)
 	}
 
-	pager := servers.List(client, ListOpts{ListOpts: servers.ListOpts{Status: "ACTIVE"}, ProjectID: projectID})
+	pager := servers.List(client, ListOpts{ListOpts: servers.ListOpts{Status: "ACTIVE"}})
 	if err := pager.Err; err != nil {
 		return nil, fmt.Errorf("discover-os: ListServers failed: %s", err)
 	}
@@ -130,12 +120,24 @@ func newClient(args map[string]string, l *log.Logger) (*gophercloud.ServiceClien
 		region = "RegionOne"
 	}
 	projectID := argsOrEnv(args, "project_id", "OS_PROJECT_ID")
+	projectName := argsOrEnv(args, "project_name", "OS_PROJECT_NAME")
 	insecure := argsOrEnv(args, "insecure", "OS_INSECURE")
 	domain_id := argsOrEnv(args, "domain_id", "OS_DOMAIN_ID")
 	domain_name := argsOrEnv(args, "domain_name", "OS_DOMAIN_NAME")
 
 	if url == "" {
 		return nil, fmt.Errorf("discover-os: Auth url must be provided")
+	}
+
+	if projectID == "" && projectName == "" { // Use the one on the instance if not provided either by parameter or env
+		l.Printf("[INFO] discover-os: ProjectID not provided. Looking up in metadata...")
+		var err error
+		projectID, err = getProjectID()
+		if err != nil {
+			return nil, err
+		}
+		l.Printf("[INFO] discover-os: ProjectID is %s", projectID)
+		args["project_id"] = projectID
 	}
 
 	ao := gophercloud.AuthOptions{
@@ -146,6 +148,7 @@ func newClient(args map[string]string, l *log.Logger) (*gophercloud.ServiceClien
 		Password:         password,
 		TokenID:          token,
 		TenantID:         projectID,
+		TenantName:       projectName,
 	}
 
 	client, err := openstack.NewClient(ao.IdentityEndpoint)
