ad82be7fe3d65d11ce2e6b074e297e02463ebbf3
Divyansh Singh <divyansh-singh@ibm.com>
2026-08-04 01:25:02 +0530

    drop vendor directory

--- provider/vsphere/vendor/github.com/vmware/govmomi/simulator/esx/doc.go
+++
@@ -1,8 +0,0 @@
-// © Broadcom. All Rights Reserved.
-// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
-// SPDX-License-Identifier: Apache-2.0
-
-/*
-Package esx contains SOAP responses from an ESX server, captured using `govc ... -dump`.
-*/
-package esx
--- provider/vsphere/vendor/github.com/vmware/govmomi/vim25/types/base.go
+++
@@ -1,7 +0,0 @@
-// © Broadcom. All Rights Reserved.
-// The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
-// SPDX-License-Identifier: Apache-2.0
-
-package types
-
-type AnyType any
